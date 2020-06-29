893c87df348b00537a117fea4b6ee498321a753d
Amarnath TSR <tsr.amarnath@gmail.com>
2020-06-29 23:24:00 +0530

    brand changes

--- src/pages/layouts/MainLayout.js
+++ src/pages/layouts/MainLayout.js
@@ -15,8 +15,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://www.denocart.com/">
+        DenoCart
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
