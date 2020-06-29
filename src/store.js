6c13278837e76aca8168be229257c07b1d553e8c
Amarnath TSR <tsr.amarnath@gmail.com>
2020-06-29 23:13:14 +0530

    store created

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './redux/reducers';
 import logger from 'redux-logger'
 const initialState = {};
 
 const middleware = [thunk, logger];
-
 const store = createStore(
   rootReducer,
   initialState,
